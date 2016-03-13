6555aaa6dfaa5a7cda49d0d2b8df3c4766235e63
myunghoon kim <myungtkim@gmail.com>
2016-03-12 20:16:55 -0800

    adds model/schemas for testing

---
+++ controllers/vursers.js
@@ -0,0 +1,22 @@
+
+// Require resource's model(s).
+var Vurser = require("../models/Vurser");
+
+var index = function(req, res, next){
+
+  Vurser.find({}, function(error, Vursers){
+    res.render('Vursers/index', {Vursers: Vursers});
+  });
+};
+
+var show = function(req, res, next){
+  Vurser.findById(req.params.id, function(error, Vurser){
+    if (error) res.json({message: 'Could not find Vurser because ' + error});
+    res.render('Vursers/show', {Vurser: Vurser});
+  });
+};
+
+module.exports = {
+  index: index,
+  show:  show
+};
---
+++ models/Song.js
@@ -0,0 +1,29 @@
+
+var mongoose = require('mongoose'),
+    debug    = require('debug')('app:models');
+
+
+var upvoteSchema = new mongoose.Schema({
+  song: {type: mongoose.Schema.Types.ObjectId,
+         ref: "Song"},
+  upvotes: Number,
+  upvotee: {type: mongoose.Schema.Types.ObjectId,
+            ref: "Vurser"}
+})
+
+
+var songSchema = new mongoose.Schema({
+  title: String,
+  artist: String,
+  album: String,
+  length: Number,
+  track_id: String,
+  upvotes_for: [upvoteSchema],
+  upvoted_to: [upvoteSchema],
+  playlists: {type: mongoose.Schema.Types.ObjectId,
+             ref: "Playlist"}
+});
+
+var Song = mongoose.model('Song', songSchema);
+
+module.exports = Song;
