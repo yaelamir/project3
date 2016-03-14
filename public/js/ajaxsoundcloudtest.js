3b7bfc05f3b83ca7eb5f241324db145fbb91e59e
Yael Amir <yaelamir1@gmail.com>
2016-03-13 21:50:55 -0700

    ajax functions

--- public/js/ajaxsoundcloudtest.js
+++ public/js/ajaxsoundcloudtest.js
@@ -1,4 +1,6 @@
-
+// var song = {
+//   query: $('input#search').val()
+// }
 
 SC.initialize({
   client_id: 'f4ddb16cc5099de27575f7bcb846636c'
@@ -8,26 +10,29 @@ SC.initialize({
 //As a user, I would like to be able to search for songs
 SC.get('/tracks', {
   // user: {username: "johnlegend"}
-  q: 'all of me'
+  q: $('input#search').val()
 })
 .then(function(tracks) {
   tracks.forEach(function(q) {
     // console.log(q.title);
-    console.log("title: ", q.title, "artist: ", q.user.username);
+    // console.log("title: ", q.title, "artist: ", q.user.username);
+    // $(ul#searchResults).append(q.title, q.user.username);
+    return q
   })
+  $searchResults.append($renderResults);
 });
 
 
-//displays users name
-//AAU I'd like to be able to search for an artist so that I can see the song that they've uploaded
-SC.get('/users', {
-  // user: {username: "johnlegend"}
-  q: 'justin bieber'
-})
-.then(function(tracks) {
-  tracks.forEach(function(q) {
-    // console.log(q.title);
-    console.log("fullname: ", q.full_name, );
-  })
-});
+// displays users name
+// AAU I'd like to be able to search for an artist so that I can see the song that they've uploaded
+// SC.get('/users', {
+//   // user: {username: "johnlegend"}
+//   q: 'justin bieber'
+// })
+// .then(function(tracks) {
+//   tracks.forEach(function(q) {
+//     // console.log(q.title);
+//     console.log("fullname: ", q.full_name);
+//   })
+// });
 
