89826affcc5983f25045ef4ff934fc4d5b375a0a
Jui-Young Chen <jyc422@gmail.com>
2016-03-14 16:31:10 -0700

    working search api call in ajazsoundcloudtest.js and minor change to index.ejs

--- public/js/ajaxsoundcloudtest.js
+++ public/js/ajaxsoundcloudtest.js
@@ -1,57 +1,36 @@
-// var song = {
-//   query: $('input#search').val()
-// }
+var renderLi;
+var $search = $('#search');
 
 SC.initialize({
   client_id: 'f4ddb16cc5099de27575f7bcb846636c',
   redirect_uri: "http://localhost:3000/auth/soundcloud/callback"
 });
 
-// var renderLi;
-//displays tracks with titles
-//As a user, I would like to be able to search for songs
-function getTracks() {
-  SC.get('/tracks', {
-  q: $('input#search').val()
-  }, function(tracks) {
-    tracks.forEach(function(track) {
-      showTracks(track);
-    });
-  })
+function getTracks(query) {
+  return SC.get('/tracks', { q: query });
 }
 
-$(document).ready(function() {
-  //SC.get('/tracks', { genres: 'foo' }, function(tracks) {
-  //  $(tracks).each(function(index, track) {
-  //    $('#results').append($('<li></li>').html(track.title + ' - ' + track.genre));
-  //  });
-  //});
-});
 
-function showTracks(track) {
-  var $trackItem = $(renderLi(track));
+function showTracks(evt) {
+  evt.preventDefault();
+  getTracks($search.val())
+    .then(function(tracks) {
+
+      renderTracks(tracks);
+    });
+}
+
+function renderTracks(tracks) {
+  var $trackItem = $(renderLi({tracks: tracks}));
+  $('li#line').remove();
   $('ul#searchResults').append($trackItem);
-  console.log($trackItem)
 }
 
 $(function() {
   renderLi = _.template(`
-    <li><%= title %></li>
+    <% tracks.forEach(function(track) { %>
+      <li id="line"><%= track.title %></li>
+    <% }); %>
   `)
-
-  $('form#search-box').on('submit', getTracks);
+  $('form#searchbox').on('submit', showTracks);
 })
-
-// displays users name
-// AAU I'd like to be able to search for an artist so that I can see the song that they've uploaded
-// SC.get('/users', {
-//   // user: {username: "johnlegend"}
-//   q: 'justin bieber'
-// })
-// .then(function(tracks) {
-//   tracks.forEach(function(q) {
-//     // console.log(q.title);
-//     console.log("fullname: ", q.full_name);
-//   })
-// });
-
