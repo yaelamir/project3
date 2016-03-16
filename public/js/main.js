cc6c21ceae546c5077d39bbd74fc48348b718c03
h4w5 <pj@ga.co>
2016-03-15 17:40:19 -0700

    restructure client app and add materialize

--- public/js/main.js
+++ public/js/main.js
@@ -1,28 +1,76 @@
 console.log('JS loaded!');
 
+var renderTrack,
+    $search;
 
-// var renderSong = _.template(`
+// Page load.
+$(function() {
+  $search = $('#search');
 
-//   `);
+  // Compile track template.
+  renderTrack = _.template(`
+    <% tracks.forEach(function(track) { %>
+      <span class="song-total" data-track-src="<%= track.stream_url %>?client_id=f4ddb16cc5099de27575f7bcb846636c">
+        <button class="song-stream <%= track.id %>">Play</button>
+          <img class="pic song-image" src="<%= track.user.avatar_url %>" style="max-width: 20px;">
+          &nbsp&nbsp
+          <%= track.title %>
+      </span>
+    <% }); %>
+  `);
 
+  // Add event handlers to page.
+  $('form#searchbox').on('submit', showTracks);
+  $('#searchResults').on('click', 'button', playSongs);
+});
 
-// var $blabsList;
-// var renderBlab = _.template(`
-//   <article id="<%= _id %>" class="blab blab-container">
-//     <a href="/blabs/<%= _id %>"><h3><%= name %></h3></a>
-//     <span>
-//       Created at <%= createdAt %>, by
-//       <a href="/users/<%= creator %>"><%= creatorHandle %></a>
-//     </span>
-//     <!-- <br> -->
-//     <!-- <button>upvote</button> VoteCount <button>downvote</button> -->
-//     <button class="blab-remove" data-blab-id="<%= _id %>" >delete</button>
-//   </article>
-// `);
+/*
+ * SoundCloud API HELPER FUNCTIONS
+ */
 
+SC.initialize({
+  client_id: 'f4ddb16cc5099de27575f7bcb846636c',
+  redirect_uri: "http://localhost:3000/auth/soundcloud/callback"
+});
 
+console.log("SoundCloud SDK initialized.");
 
-// //TO GO IN INDEX.EJS
-// <ul class="top-three-songs" id="top-songs">
+function getTracks(query) {
+  return SC.get('/tracks', { q: query });
+}
 
-// </ul>
+/*
+ * AJAX / RENDER FUNCTIONS =============================================
+ */
+
+function showTracks(evt) {
+  evt.preventDefault();
+
+  getTracks($search.val())
+    .then(function(tracks) {
+      renderTracks(tracks);
+    });
+}
+
+function renderTracks(tracks) {
+  var $trackItem = $(renderTrack({tracks: tracks}));
+  $('span.song-total').remove();
+  $('img.song-image').remove();
+  $('section#searchResults').append($trackItem);
+}
+
+/*
+ * PAGE INTERACTIONS ===================================================
+ */
+
+function playSongs() {
+  var $audio  = $('#audio-player');
+  var playUri = $(this).parent().data('track-src');
+
+  console.log(playUri);
+  $audio.prepend(`<source src="${playUri}" type="audio/mpeg" />`);
+
+  $audio[0].pause();
+  $audio[0].load(); // suspends and restores all audio element
+  $audio[0].play();
+}
